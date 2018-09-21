5b8d65deef8fb654e98cb7533114bc22a56ae7ac
Humac <hoomaac@gmail.com>
2018-09-21 11:13:13 +0430

    login and logout added

--- web/urls.py
+++ web/urls.py
@@ -2,7 +2,9 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^index/$', view=views.index, name='index'),
-    url(r'^login$', view=views.login, name='login'),
-    url(r'^register$', view=views.register, name='register'),
+    url(r'^$', view=views.index, name='index'),
+    # url(r'^index/$', view=views.index, name='index'),
+    url(r'^login/$', view=views.LoginView.as_view(), name='login'),
+    url(r'^register/$', view=views.register, name='register'),
+    url(r'^logout/$', view=views.logout_user, name='logout'),
 ]
--- web/views.py
+++ web/views.py
@@ -2,29 +2,43 @@ from . import models
 from django.http import HttpResponse
 from . import forms
 from django.shortcuts import render, redirect
-from django.contrib.auth import login, authenticate
+from django.contrib.auth import login, authenticate, logout
 from django.contrib.auth.forms import AuthenticationForm
 from django.views import generic
-from django.core.urlresolvers import reverse_lazy
+from django.urls import reverse_lazy
+from django.contrib.auth.decorators import login_required
 
-class LoginView(generic.FormView):
-    form_class = AuthenticationForm
-    success_url = reverse_lazy('index')
-    template_name = 'index.html'
-
-    def get_form(self, form_class = None):
-        if form_class is None:
-            form_class = self.get_form_class()
-        
-        return form_class(self.request,**self.get_form_kwargs())
-
-    
 
 
 def index(request):
     return render(request, 'index.html')
     
 
+
+class LoginView(generic.FormView):
+    form_class = AuthenticationForm
+    success_url = reverse_lazy('index')
+    template_name = 'web/login.html'
+ 
+    def get_form(self, form_class = None):
+        if form_class is None:
+            form_class = self.get_form_class()
+        
+        return form_class(self.request,**self.get_form_kwargs())  
+    
+
+    def form_valid(self, form):
+        login(self.request, form.get_user()) 
+        return super().form_valid(form)
+
+
+@login_required
+def logout_user(request):
+    logout(request)
+    return redirect("index")
+
+
+ 
 def register(request):
 
     if request.method == 'POST':
